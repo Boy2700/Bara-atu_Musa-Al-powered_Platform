bd3d9d1d8d756d6861762c332013a5d93f12492f
Yusuf Babatunde Yusuf <105415149+Boy2700@users.noreply.github.com>
2024-05-10 04:37:11 -0700

    Ai

--- wordmeaning/script.js
+++ wordmeaning/script.js
@@ -84,7 +84,7 @@ function logout() {
 function goToHome() {
     // Redirect to the home page
     alert("Welcome to Home Page")
-    window.location.href = "./Bara-atu_Musa-Al-powered_Platform/home.html";
+    window.location.href = "https://boy2700.github.io/Bara-atu_Musa-Al-powered_Platform/home.html";
 }
 
 function clearFields() {
